42dc4caa897d3962079eb01a2be56396027a8ed8
Matt Brendzel <matt.brendzel@gmail.com>
2020-04-05 16:41:49 -0400

    Implement CallRouter#calculate_match_score

--- ruby/call_router.rb
+++ ruby/call_router.rb
@@ -7,4 +7,16 @@ class CallRouter
   def initialize(agents)
     @agents = agents
   end
+
+  def calculate_match_score(agent, consumer)
+    # For Agents, range attributes and set attributes follow different
+    # naming conventions.
+    ranges_count = %w[age num_kids num_cars income].map do |attr|
+      agent.send("#{attr}_range").include? consumer.send(attr)
+    end.count(true)
+    sets_count = %w[us_state residency_type].map do |attr|
+      agent.send("#{attr}s_served").include? consumer.send(attr)
+    end.count(true)
+    ranges_count + sets_count
+  end
 end
