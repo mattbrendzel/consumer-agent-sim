3a9880ad898554eb532c6c2863364f09f28083f7
Matt Brendzel <matt.brendzel@gmail.com>
2020-04-05 17:32:05 -0400

    Give agents a unique ID field
    This will simplify testing.

--- ruby/agent.rb
+++ ruby/agent.rb
@@ -14,6 +14,21 @@ end
 class Agent
   attr_reader :age_range, :num_kids_range, :num_cars_range, :income_range
   attr_reader :us_states_served, :residency_types_served # Sets, not ranges
+  attr_reader :agent_id
+
+  ## Class level
+
+  @next_agent_id = 0
+
+  # Make 'next_phone_number' publicly readable
+  class << self
+    attr_reader :next_agent_id
+  end
+
+  # Rather than allowing any kind of edit, only allow increments.
+  def self.increment_next_agent_id
+    @next_agent_id += 1
+  end
 
   def initialize
     @age_range = random_range_within(18, 100)
@@ -22,5 +37,8 @@ class Agent
     @num_cars_range = random_range_within(0, 4)
     @residency_types_served = %w[renter owner].sample(rand(1..2))
     @income_range = random_range_within(20, 200)
+    # Give agents a unique identifier as well
+    @agent_id = self.class.next_agent_id
+    self.class.increment_next_agent_id
   end
 end
