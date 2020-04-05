b538aa8b9cafc17301b04836d38040528177567c
Matt Brendzel <matt.brendzel@gmail.com>
2020-04-05 13:19:14 -0400

    Define props for CallRouter, Simulator classes
    The call router will need to be able to look through the list of agents,
    so it makes sense to have that be a property. The simulator as a whole
    consists of a set of consumers, a set of agents, and a call router,
    so make those all properties; however, rather than do this in the
    `initialize` method, create a separate method that can be called
    repeatedly for a given simulator instance.

--- ruby/simulation.rb
+++ ruby/simulation.rb
@@ -56,10 +56,28 @@ class Agent
   end
 end
 
+# CallRouter : Receives calls from Consumers and passes them to Agents
 class CallRouter
+  def initialize(agents)
+    @agents = agents
+  end
 end
 
+# Simulator : Manages the simulation at the top level
 class Simulator
+  attr_reader :consumers, :agents, :call_router
+
+  def initialize
+    set_start_conditions
+  end
+
+  def set_start_conditions
+    @consumers = Array.new(5) { Consumer.new }
+    @agents = Array.new(2) { Agent.new }
+    @call_router = CallRouter.new(@agents)
+  end
 end
 
+simulator = Simulator.new
+simulator.set_start_conditions
 binding.pry
